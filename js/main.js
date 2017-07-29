9519a613491116889388464983a292b66a27c6cb
batel ismailov <batelsos123@gmail.com>
2017-07-29 17:07:54 +0300

    index header
    * added text file with all the sql code
    * get the personal user info from the database [done]
    -> REMEMBER
    change the php files to get user data from database using the user ID
    and not NAME
    :+1:

--- js/main.js
+++ js/main.js
@@ -1,27 +1,27 @@
 $(function(){
-
-    var userName = window.location.hash.substr(1); //Get the section after hash tag from URL, e.g. index.html#Arik return 'Arik'
-    if(userName === '') {
+  var userName = "batel";
+    var userId = window.location.hash.substr(1); //Get the section after hash tag from URL, e.g. index.html#Arik return 'Arik'
+    if(userId === '') {
         alert('No selected');
     } else {
 
-      $.get('/final-project-fullstack/php/myuser.php?user=' + userName, function(data) { //This code makes an HTTP request to /arik and puts the data in the 'data' variable
+      $.get('/FinalProject/php/myuser.php?user=' + userId, function(data) { //This code makes an HTTP request to /arik and puts the data in the 'data' variable
 
-          console.log('Got data', data); //We just print whatever we got from the server
+          console.log('user data:', data); //We just print whatever we got from the server
 
           //Basic info
-          $('#intro header h1').text(data.basicInfo.firstName); //Let's use the data from server and pub it on page!
-          $('#intro header h2').text(data.basicInfo.lastName);
-          $('#intro header').append('<h3>'+data.basicInfo.title+'</h3>')
+          $('#intro header h1').text(data[0].firstname); //Let's use the data from server and pub it on page!
+          $('#intro header h2').text(data[0].lastname);
+          $('#intro header').append('<h3>'+data[0].title+'</h3>')
 
-          $('#intro-me').append('<li>'+data.phone+'</li>')
-          $('#intro-me').append('<li>'+data.area+'</li>')
-          $('#intro-me').append('<li>'+data.email+'</li>')
-		  
+          $('#intro-me').append('<li>'+data[0].phone+'</li>')
+          $('#intro-me').append('<li>'+data[0].area+'</li>')
+          $('#intro-me').append('<li>'+data[0].email+'</li>')
+
 		  //aboutme
-		  $('#about p').text(data.about);
-		  
-		  
+		  $('#about p').text(data[0].about);
+
+
           //Social networks
           $ul = $('<ul>', {
               'id': 'social-networks'
@@ -43,106 +43,106 @@ $(function(){
 
           $ul.insertAfter('#intro');
 
-		
+
       });
-	  
-	  	$.get('/final-project-fullstack/php/proskills.php?user=' + userName, function(data) 
+
+	  	$.get('/final-project-fullstack/php/proskills.php?user=' + userName, function(data)
        { //This code makes an HTTP request to /arik and puts the data in the 'data' variable
-			
+
           console.log('Got data', data); //We just print whatever we got from the server
-		  
+
 		  //clear "loading" text
 		  $('#skillname').empty();
 		  $('#skillvalue').empty();
-		  
-		  
-		  for ($x = 0; $x <= data.length; $x++) 
+
+
+		  for ($x = 0; $x <= data.length; $x++)
           {
 			$('#skillname').append('<li>'+data[$x].skillname+'</li>');
 			$('#skillvalue ').append('<li><progress max=100 value='+data[$x].skillvalue+'></progress></li>');
           }
        });
-    
-        
-          $.get('/final-project-fullstack/php/perskills.php?user=' + userName, function(data) 
+
+
+          $.get('/final-project-fullstack/php/perskills.php?user=' + userName, function(data)
          { //This code makes an HTTP request to /arik and puts the data in the 'data' variable
-			
+
           console.log('Got data', data); //We just print whatever we got from the server
 		  //clear "loading" text
 		  $('#skillnameper').empty();
 		  $('#skillvalueper').empty();
-		 
-		 
+
+
 		  for ( $x=0;$x <= data.length; $x++) {
 			$('#skillnameper').append('<li>'+data[$x].skillname+'</li>');
 			$('#skillvalueper ').append('<li><progress max=100 value='+data[$x].skillvalue+'></progress></li>');
 			}
 		});
-        
-         $.get('/final-project-fullstack/php/experience.php?user=' + userName, function(data) 
+
+         $.get('/final-project-fullstack/php/experience.php?user=' + userName, function(data)
          { //This code makes an HTTP request to /arik and puts the data in the 'data' variable
-			
+
           console.log('Got data', data); //We just print whatever we got from the server
 		  //clear "loading" text
 		  $('#blueTitle').empty();
 		  $('#Title').empty();
           $('#date').empty();
-		  $('#paragraph').empty();     
-		 
-		 
+		  $('#paragraph').empty();
+
+
 		  for ( $x=0;$x <= data.length; $x++) {
 			$('#blueTitle').append('<li>'+data[$x].blueTitle+'</li>');
 			$('#Title').append('<li>'+data[$x].Title+'</li>');
             $('#date').append('<li>'+data[$x].date+'</li>');
-            $('#paragraph').append('<li>'+data[$x].paragraph+'</li>');  
+            $('#paragraph').append('<li>'+data[$x].paragraph+'</li>');
 			}
 		});
-        
-         $.get('/final-project-fullstack/php/education.php?user=' + userName, function(data) 
+
+         $.get('/final-project-fullstack/php/education.php?user=' + userName, function(data)
          { //This code makes an HTTP request to /arik and puts the data in the 'data' variable
-			
+
           console.log('Got data', data); //We just print whatever we got from the server
 		  //clear "loading" text
 		  $('#blueTitle').empty();
 		  $('#Title').empty();
           $('#date').empty();
-		  $('#paragraph').empty();     
-		 
-		 
+		  $('#paragraph').empty();
+
+
 		  for ( $x=0;$x <= data.length; $x++) {
 			$('#blueTitle').append('<li>'+data[$x].blueTitle+'</li>');
 			$('#Title').append('<li>'+data[$x].Title+'</li>');
             $('#date').append('<li>'+data[$x].date+'</li>');
-            $('#paragraph').append('<li>'+data[$x].paragraph+'</li>');  
+            $('#paragraph').append('<li>'+data[$x].paragraph+'</li>');
 			}
 		});
-        
-        $.get('/final-project-fullstack/php/hobbies.php?user=' + userName, function(data) 
+
+        $.get('/final-project-fullstack/php/hobbies.php?user=' + userName, function(data)
          { //This code makes an HTTP request to /arik and puts the data in the 'data' variable
-			
+
           console.log('Got data', data); //We just print whatever we got from the server
 		  //clear "loading" text
 		  $('#photo').empty();
 		  $('#topic').empty();
 
-		 
+
 		  for ( $x=0;$x <= data.length; $x++) {
 			$('#photo').append('<li>'+data[$x].photo+'</li>');
 			$('#topic').append('<li>'+data[$x].topic+'</li>');
- 
+
 			}
-		}); 
-        
-        
-          $.get('/final-project-fullstack/php/language.php?user=' + userName, function(data) 
+		});
+
+
+          $.get('/final-project-fullstack/php/language.php?user=' + userName, function(data)
          { //This code makes an HTTP request to /arik and puts the data in the 'data' variable
-			
+
           console.log('Got data', data); //We just print whatever we got from the server
 		  //clear "loading" text
 		  $('#languageName').empty();
 		  $('#languageValue').empty();
-		 
-		 
+
+
 		  for ( $x=0;$x <= data.length; $x++) {
 			$('#languageName').append('<li>'+data[$x].languageName+'</li>');
 			$('#languageValue ').append('<li><progress max=100 value='+data[$x].languageValue+'></progress></li>');
--- php/myuser.php
+++ php/myuser.php
@@ -2,33 +2,30 @@
 require_once('db.php');
 $data=[];
 if (isset($_GET) && !empty($_GET)){
-		$query = sprintf("select * from users where name = '%s'",$_GET['user']);
+		$query = sprintf("select * from users where id = '%s'",$_GET['user']);
 		$selectResult = $mysqli->query($query);
-		
+
 		if ($selectResult->num_rows) {
 				while ($row = $selectResult->fetch_assoc()) {
-					
-					 $data = [
-										 "basicInfo" => [
-										 "firstName" => $row['firstname'],
-										 "lastName"	=> $row['lastname'],
-										 "title"	=> $row['title']
-									   ],
-							"name" => $row['name'],
+					$data[] = $row;
+					/* $data = [
+							 "firstName" => $row['firstname'],
+							 "lastName"	=> $row['lastname'],
+							 "title"	=> $row['title'],
 							"email" => $row['email'],
 							"phone" => $row['phone'],
 							"area" => $row['area'],
 							"about" => $row['about']
-						   ];
-					
+						];*/
+
 				}
 		}
 }
 
    header('Content-Type: application/json');
    echo json_encode($data);
- 
 
-   
+
+
 
 ?>
